f1920336ae994780bd7222b40248627cdd86274c
mohammadbismail <m.basim.ismail@gmail.com>
2022-10-25 13:08:13 +0300

    submit

--- CafeUtil.java
+++ CafeUtil.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+
 public class CafeUtil {
-    public int getStreakGoal(){
-        
+    public int getStreakGoal() {
+        int sum = 0;
+        int[] myArr = new int[10];
+        for (int i = 0; i < myArr.length; i++) {
+            sum = +i;
+        }
+        return sum;
     }
+
+    public double getOrderTotal(double[] prices) {
+        double sum = 0;
+        for (int i = 0; i < prices.length; i++) {
+            sum += prices[i];
+        }
+        return sum;
+    }
+
+    public static void displayMenu(ArrayList<String> menuItems) {
+        // for (String item : menuItems) {
+        // System.out.println(item);
+        // }
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.println(i + " " + menuItems.get(i));
+        }
+    }
+
+    public void addCustomer(ArrayList<String> customers) {
+        System.out.println("Please enter your name");
+        String userName = System.console().readLine();
+        System.out.println("Hello," + userName);
+        String formattedOutput = String.format("There are %x people infront of you", customers.size());
+        System.out.println(formattedOutput);
+    }
+
 }
--- TestCafe.java
+++ TestCafe.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class TestCafe {
     public static void main(String[] args) {
-        
+        CafeUtil cafeInstance = new CafeUtil();
+        // Display sum of numbers
+        int myStreakGoal = cafeInstance.getStreakGoal();
+        // Sum total of numbers
+        double[] myOrders = { 10.2, 3, 5, 6, 6 };
+        double myTotalOrder = cafeInstance.getOrderTotal(myOrders);
+        // Items to printout
+        ArrayList<String> myList = new ArrayList<String>();
+        myList.add("drip coffee");
+        myList.add("cappuccino");
+        myList.add("latte");
+        myList.add("mocha");
+
+        // Testing
+        // System.out.println(myStreakGoal);
+        // System.out.println(myTotalOrder);
+        // cafeInstance.displayMenu(myList);
+        cafeInstance.addCustomer(myList);
+
     }
 }
